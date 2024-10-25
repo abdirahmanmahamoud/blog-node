1f4f880780d7591a7fe9a8dceb17c2c084e5c5ca
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2024-10-25 15:16:03 +0300

    lesson 3

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,3 +1,113 @@
+import db from "../../lib/db.js";
+
 export const postCreate = (req, res) => {
-  return res.status(200).json({ message: "Hello World" });
+  const { title, description, image } = req.body;
+
+  if (!title || !description || !image) {
+    return res.status(400).json({ message: "Please enter all fields" });
+  }
+
+  const query = `INSERT INTO post (title, description,image) VALUES ('${title}', '${description}', '${image}')`; // Prepared statement with placeholder
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Database error" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Post created successfully", postId: result.insertId });
+  });
+};
+
+export const selectPosts = (req, res) => {
+  const query = `SELECT * FROM post`;
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Database error" });
+    }
+    return res.status(200).json(result);
+  });
+};
+
+export const selectPostById = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ message: "required param id" });
+  }
+
+  const query = `SELECT * FROM post WHERE id = ${id}`;
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Database error" });
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    return res.status(200).json(result[0]);
+  });
+};
+
+export const postUpdate = (req, res) => {
+  const { id } = req.params;
+  const { title, description, image } = req.body;
+
+  if (!id || !title || !description || !image) {
+    return res.status(400).json({ message: "Please enter all fields" });
+  }
+
+  const query = `SELECT * FROM post WHERE id = ${id}`;
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Database error" });
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const query = `UPDATE post SET title = '${title}', description = '${description}', image = '${image}' WHERE id = ${id}`;
+    db.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Database error" });
+      }
+      return res.status(200).json({ message: "Post updated successfully" });
+    });
+  });
+};
+
+export const postDelete = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ message: "required param id" });
+  }
+
+  const query = `SELECT * FROM post WHERE id = ${id}`;
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Database error" });
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const query = `DELETE FROM post WHERE id = ${id}`;
+    db.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Database error" });
+      }
+      return res.status(200).json({ message: "Post deleted successfully" });
+    });
+  });
 };
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { postCreate } from "../controllers/post.js";
+import {
+  postCreate,
+  postDelete,
+  postUpdate,
+  selectPostById,
+  selectPosts,
+} from "../controllers/post.js";
 import { middlewareAdmin } from "../../lib/middlewares.js";
 
 const router = express.Router();
 
-router.post("/add", middlewareAdmin, postCreate);
-
-// router.post("/signup", signup);
+router.post("/", middlewareAdmin, postCreate);
+router.get("/", selectPosts);
+router.get("/:id", selectPostById);
+router.put("/:id", middlewareAdmin, postUpdate);
+router.delete("/:id", middlewareAdmin, postDelete);
 
 export default router;
